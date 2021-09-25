703582d15075a5b268004225fa3e1a5144b48ccf
Joseph Emmanuel <joseph.emmanuel@outlook.com>
2021-09-25 16:47:42 -0400

    Completed till material UI icons import

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Switch }from "react-router-dom";
+
+import Home from "./Home"
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
+      </Router>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+// import './';
+
+function Home() {
+    return (
+        <div className="home">Hello</div>
+    )
+}
+
+export default Home
